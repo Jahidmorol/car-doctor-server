6e87257a8deef22c79cbdbadb9d26617e5d3db78
jahid morol <jahidmorol2@gmail.com>
2023-05-14 00:41:32 +0600

    added jwt in this project for security

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
 const port = process.env.PORT || 5000;
@@ -20,17 +21,48 @@ const client = new MongoClient(uri, {
   },
 });
 
+const verifyJWT = (req, res, next) => {
+  // console.log('hiting verifyJWT',req.headers.authorization);
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res
+      .status(401)
+      .send({ error: true, message: "unauthorized access" });
+  }
+  const token = authorization.split(" ")[1];
+  jwt.verify(token, process.env.JWT_PRIVATE_KEY, (err, decoded) => {
+    // console.log('jahid::::.....',decoded);
+    if (err) {
+      return res
+        .status(401)
+        .send({ error: true, message: "unauthorized access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    // const database = client.db("sample_mflix");
-    // const movies = database.collection("movies");
-
     const serviceCollection = client.db("carDoctors").collection("services");
     const checkOutCollection = client.db("carDoctors").collection("checkouts");
 
+    // ----------------> jwt <-------------------
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      // console.log(user);
+
+      const token = jwt.sign(user, process.env.JWT_PRIVATE_KEY, {
+        expiresIn: "1h",
+      });
+      // console.log(token);
+      res.send({ token });
+    });
+
+    // ---------------allservice --------------
     app.get("/services", async (req, res) => {
       const cursor = serviceCollection.find();
       const result = await cursor.toArray();
@@ -49,45 +81,52 @@ async function run() {
     });
 
     //---------------- checkOut ------------
-
-    app.post('/checkout', async (req, res)=> {
+    app.post("/checkout", async (req, res) => {
       const order = req.body;
       // console.log(order);
-      const result = await checkOutCollection.insertOne(order)
-      res.send(result)
-    })
+      const result = await checkOutCollection.insertOne(order);
+      res.send(result);
+    });
 
     // ---------------orederReviews------------
-    app.get('/orderreviews', async(req, res) => {
+    app.get("/orderreviews", verifyJWT, async (req, res) => {
       // console.log(req.query.email);
+
+      // console.log('after rerifyJWT',req.decoded);
+      if(req.decoded.email !== req.query?.email){
+        return res.status(403).send({error: true, message: 'forbidden access'})
+      }
+
       let query = {};
-      if(req.query?.email){
-        query = {email: req.query.email}
+      if (req.query?.email) {
+        query = { email: req.query.email };
       }
       const result = await checkOutCollection.find(query).toArray();
-      res.send(result)
-    })
+      res.send(result);
+    });
 
-    app.patch('/orderreviews/:id', async(req, res) => {
+    
+    app.patch("/orderreviews/:id", async (req, res) => {
       const id = req.params.id;
-      const body = req.body
-      const filter = {_id: new ObjectId(id)}
+      const body = req.body;
+      const filter = { _id: new ObjectId(id) };
       const approvedStatus = {
         $set: {
-          status: body.status
+          status: body.status,
         },
       };
-      const result = await checkOutCollection.updateOne(filter,approvedStatus)
-      res.send(result)
+      const result = await checkOutCollection.updateOne(filter, approvedStatus);
+      res.send(result);
+    });
 
-    })
-
-    app.delete('/orderreviews/:id', async(req, res) => {
+    app.delete("/orderreviews/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)}
-      const result = await checkOutCollection.deleteOne(query)
-      res.send(result)
-    })
+      const query = { _id: new ObjectId(id) };
+      const result = await checkOutCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // -----------------------------------------------
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
